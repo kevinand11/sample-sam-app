7ed02730a46c446622ee6aefc698775aaf519432
kevinand11 <kevinfizu@gmail.com>
2023-04-17 11:19:12 +0100

    add hello world handler

--- hello-world/app.ts
+++ hello-world/app.ts
@@ -1,10 +1,29 @@
 import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda'
 
+export const lambdaHandler = async (_: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+    try {
+        return {
+            statusCode: 200,
+            body: JSON.stringify({
+                message: 'hello world',
+            }),
+        }
+    } catch (err) {
+        console.log(err)
+        return {
+            statusCode: 500,
+            body: JSON.stringify({
+                message: 'some error happened',
+            }),
+        }
+    }
+}
+
 export const parseHeader = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
     const header = event.headers['x-client-id']
     const headerPresent = header !== undefined
     return {
         statusCode: 200,
-        body: JSON.stringify(headerPresent)
+        body: JSON.stringify(headerPresent),
     }
 }
