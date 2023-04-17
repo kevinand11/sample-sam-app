cb2c3d814d243abf8197719a319ee5e3beb2b1ff
kevinand11 <kevinfizu@gmail.com>
2023-04-17 11:57:34 +0100

    install middy middlewares

--- hello-world/app.ts
+++ hello-world/app.ts
@@ -1,3 +1,5 @@
+import middy from '@middy/core'
+import jsonBodyParser from '@middy/http-json-body-parser'
 import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda'
 
 export const lambdaHandler = async (_: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
@@ -19,11 +21,13 @@ export const lambdaHandler = async (_: APIGatewayProxyEvent): Promise<APIGateway
     }
 }
 
-export const parseHeader = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
-    const header = event.headers['x-client-id']
-    const headerPresent = header !== undefined
-    return {
-        statusCode: 200,
-        body: JSON.stringify(headerPresent),
-    }
-}
+export const parseHeader = middy()
+    .use(jsonBodyParser()) // parses the request body when it's a JSON and converts it to an object
+    .handler(async (event) => {
+        const header = event.headers['x-client-id']
+        const headerPresent = header !== undefined
+        return {
+            statusCode: 200,
+            body: JSON.stringify(headerPresent),
+        }
+    })
